77a152f73536b8672ee297e710c875d7c5ebe5cd
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2019-12-11 17:09:34 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config' )
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.urlencoded({extended: true}));
+//app.use(express.json());
+ 
+
+//Import routes
+
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute); 
+
+
+//ROUTES  
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,  
+    { useNewUrlParser: true },
+    () => console.log('connected to DB!')
+);
+
+
+app.listen(3000);
