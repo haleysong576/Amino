6934d7fc87b510bd55b2ed9c996ad1844b193c69
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2019-12-19 17:12:20 -0500

    Create DailyTrackerScreen.js

---
+++ 4Menus/DailyTrackerScreen.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {View, Image, StyleSheet, Button, Text} from "react-native";
+
+class DailyTrackerScreen extends Component{
+
+    static navigationOptions = {
+        title: 'Daily Tracker'
+      }
+
+      render(){
+
+        return(
+          <View>
+            
+          </View>
+        );
+
+      }
+
+}
+
+export default DailyTrackerScreen;
