0fcbb4acf8900e996ecd3eecdb00a023799c104a
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2019-12-19 17:12:42 -0500

    Create SettingScreen.js

---
+++ 4Menus/SettingScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import {View, Image, StyleSheet, Button} from "react-native";
+
+class Settings extends Component{
+
+    static navigationOptions = {
+        title: 'Settings'
+      }
+
+    render(){
+
+        return(
+            <View></View>
+        );
+    }
+}
+
+export default Settings;
