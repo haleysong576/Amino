824157ced604988d5da9ad98040ecaf473562b19
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2020-01-07 09:42:36 -0500

    Update SettingScreen.js

--- 4Menus/SettingScreen.js
+++ 4Menus/SettingScreen.js
@@ -7,12 +7,62 @@ class Settings extends Component{
         title: 'Settings'
       }
 
+     onPressNotificatonButton(){
+      this.props.navigation.navigate('Notification');
+    }
+    onPressPasswordButton(){
+        this.prop.navigation.navigate('Password');
+    }
+    onPressLogOutButton(){
+        this.prop.navigation.navigate('Log Out');
+    }
     render(){
 
-        return(
-            <View></View>
-        );
+      return(
+        <View style = {styles.container}>
+          <View style= {styles.item}>
+            <Button 
+            onPress = {() => this.onPressNotificationButton()}
+            title = "Notification"
+            />          
+          </View>
+          <View style= {styles.item}>
+            <Button 
+            onPress = {() => this.onPressPasswordButton()}
+            title = "Change Password"
+            />
+          </View>
+          <View style= {styles.item}>
+            <Button 
+            onPress = {() => this.onPressLogOutButton()}
+            title = "Log out"
+            />
+          </View>
+        </View>
+      );
+
     }
 }
 
 export default Settings;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  bigTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+});
